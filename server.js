6fea114434f8f468e3fbf7d1cf43a994b5f6bdb9
Andrew Steele <104460430+drewsteele408@users.noreply.github.com>
2025-10-09 15:08:17 -0600

    prepare activities completed

--- server.js
+++ server.js
@@ -5,11 +5,13 @@
 /* ***********************
  * Require Statements
  *************************/
+const env = require("dotenv").config()
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
-const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const inventoryRoute = require("./routes/inventoryRoute")
+const baseController = require("./controllers/baseController")
 
 /* ***********************
  * View Engine and Templates
@@ -24,10 +26,9 @@ app.set("layout", "./layouts/layout")
  *************************/
 app.use(static)
 // Index route
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
-})
-
+app.get("/", baseController.buildHome)
+// Inventory routes
+app.use("/inv", inventoryRoute)
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
